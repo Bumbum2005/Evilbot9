6b105d5bf2fef74e4714f082fce4b9f7a99f244a
Bumbum2005 <158068027+Bumbum2005@users.noreply.github.com>
2024-01-29 17:04:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.ownernomer = "2348131165984" //ur owner number2
-global.ownername = "😈𝐌𝐈𝐂𝐇𝐄𝐀𝐋 𝐊𝐈𝐍𝐆𝐒😈" //ur owner name
+global.ownernomer = "2349156305896" //ur owner number2
+global.ownername = "😎𝑨𝑺𝑲_𝑶𝑭_𝑩𝑼𝑴𝑩𝑼𝑴😎" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: iamevilboy" //ur github or insta name
 global.location = "nigeria, lagos, abuja" //ur location
 
 //new
-global.botname = "😈𝐌𝐈𝐂𝐇𝐀𝐄𝐋 𝐊𝐈𝐍𝐆𝐒 𝐁𝐎𝐓😈"
-global.ownernumber = '2348131165984'
-global.ownername = '😈𝐌𝐈𝐂𝐇𝐄𝐀𝐋 𝐊𝐈𝐍𝐆𝐒😈'
-global.ownerNumber = ["2348131165984@s.whatsapp.net"]
+global.botname = "😎𝑨𝑺𝑲_𝑶𝑭_𝑩𝑼𝑴𝑩𝑼𝑴😎"
+global.ownernumber = '2349156305896'
+global.ownername = '😎𝑨𝑺𝑲_𝑶𝑭_𝑩𝑼𝑴𝑩𝑼𝑴😎'
+global.ownerNumber = ["2349156305896@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtube.com/@DGXeon"
-global.wagc = "https://chat.whatsapp.com/HQ26izcpZ7yAWXm2yyExSX"
-global.themeemoji = '😈'
+global.wagc = "https://chat.whatsapp.com/C9eCm4qbKQQ254OduaLimt"
+global.themeemoji = '😎𝑨𝑺𝑲_𝑶𝑭_𝑩𝑼𝑴𝑩𝑼𝑴😎'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "😈•"
-global.author = "𝑨𝑵𝑻𝑰𝑺𝑶𝑪𝑰𝑨𝑳 𝑯𝑼𝑴𝑰𝑫𝑰𝑻𝒀•😈\n\n"
-global.creator = "2348131165984@s.whatsapp.net"
-global.prefa = ['😈','!','😈','#','&']
+global.packname = "😎𝑨𝑺𝑲_𝑶𝑭_𝑩𝑼𝑴𝑩𝑼𝑴😎"
+global.author = "😎𝑨𝑺𝑲_𝑶𝑭_𝑩𝑼𝑴𝑩𝑼𝑴😎"
+global.creator = "2349156305896@s.whatsapp.net"
+global.prefa = ['!','!','😎','#','&']
 global.sessionName = 'session'
 global.hituet = 0
 
